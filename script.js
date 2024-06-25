9fc46d62564934b5b958561d49b570f127e4d729
DasvinKumarR <dasvinkumar2404@gmail.com>
2024-06-25 17:17:25 +0530

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 // Declared base url
 const baseURL = "https://cataas.com/";
+let catCount = 50;
+let id = [];
 // get div element using classname
-let div = document.getElementsByClassName("content")[0];
-// promise function to fetch and display the data
-async function getData(endPoint) {
+let root = document.getElementsByClassName("root")[0];
+// function to create card.
+async function createCard(id, tags, img) {
     try {
-        div.innerText = "Loading..."
-        let response = await fetch(baseURL + endPoint);
-        if (!response.ok) {
-            throw new Error('Failed to fetch cat image');
-        }
-        let imageUrl = response.url;
-        div.innerHTML = `<img src="${imageUrl}" alt="Cat Image">`;
+        let cardWrapper = document.createElement("div");
+        cardWrapper.setAttribute("class", "card-wrapper");
+        cardWrapper.innerHTML = `<div class="card">
+        <img src="${img}" class="card-img-top gallery" alt="Cat Image Loading...">
+        <div class="card-body">
+            <h5 class="card-title">Cat Detail</h5>
+            <p class="card-text">Cat Id: ${id}</p>
+            <p class="card-text">Cat Tag: ${tags.join(', ')}</p>
+        </div>
+        </div>`;
+
+        root.appendChild(cardWrapper);
+    } catch (error) {
+        console.log(error);
+    }
+}
+// promise function to fetch and display the data
+fetch('https://cataas.com/api/cats')
+.then(response=>response.json())
+.then(data=>{
+    data.forEach(e => {
+        createCard(e._id,e.tags,baseURL+"cat/"+e._id);
+    });
+})
+.catch((err)=>console.error(err))
+//funciton to display cat images
+async function fetchData(endPoint) {
+    try {
+        root.innerHTML = `<div class="content">
+            <img class="singleImage" src="${baseURL+endPoint}" alt="Cat Loading..."/>
+        </div>`;
     } catch (error) {
         console.error('Error fetching cat image:', error);
     }
 }
+
 // function call to get cat image
 function getCat(){
-    getData("cat");
+    fetchData("cat");
 }
 // function call to get orange tag cat image
 function getOrangeCat(){
-    getData("cat/orange,cute");
+    fetchData("cat/orange,cute");
 }
 // functional to get cat gif
 function getCatGif(){
-    getData("cat/gif");
+    fetchData("cat/gif");
 }
 // function to call hello cat image
 function sayHelloCat(){
-    getData("cat/cute/says/hello");
+    fetchData("cat/cute/says/hello");
 }
